acd6f4cc1c3bc0c00daf8df195678016ad59ae5f
Killian DOUBRE <killian.doubre@etu.unilim.fr>
2021-01-22 19:11:36 +0100

    Initial commit

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import { Application } from '../declarations';
+import recipes from './recipes/recipes.service';
+import ingredients from './ingredients/ingredients.service';
+// Don't remove this comment. It's needed to format import lines nicely.
+
+export default function (app: Application): void {
+  app.configure(recipes);
+  app.configure(ingredients);
+}
---
+++ src/services/recipes/recipes.service.ts
@@ -0,0 +1,26 @@
+// Initializes the `recipes` service on path `/recipes`
+import { ServiceAddons } from '@feathersjs/feathers';
+import { Application } from '../../declarations';
+import { Recipes } from './recipes.class';
+import hooks from './recipes.hooks';
+
+// Add this service to the service type index
+declare module '../../declarations' {
+  interface ServiceTypes {
+    'recipes': Recipes & ServiceAddons<any>;
+  }
+}
+
+export default function (app: Application): void {
+  const options = {
+    paginate: app.get('paginate')
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/recipes', new Recipes(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('recipes');
+
+  service.hooks(hooks);
+}
